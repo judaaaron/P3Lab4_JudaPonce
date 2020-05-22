f4e3dfb5785945dce69b5c6c36412648f0f0cf6c
Juda <judaponce14@unitec.edu>
2020-05-22 16:11:08 -0600

    sigo improvisando pero ya va dando

--- P3Lab4_JudaPonc.cpp
+++ P3Lab4_JudaPonc.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 string nombre, apellido, password;
+int personaIngresada;
 
 vector <persona> personas;
 int main(int argc, char** argv) {
+	persona* prtpersona=NULL;
 	bool exit = true;
 		while(exit){
 		
@@ -26,11 +28,44 @@ int main(int argc, char** argv) {
 			cin>> password;
 			cout<<" Ha sido registrado con exito: "<<endl;
 			personas.push_back(persona(nombre,apellido,password));	
-			cout<<" Su usuario es: ";
-		//	for(int = 0)
+			cout<<" Su usuario es: "<<endl;
+			cout << " Nombre: "<<personas[personas.size()-1].getNombre()<<endl;
+			cout << " Apellido: "<<personas[personas.size()-1].getApellido()<<endl;
+			cout << " Contrasena: "<<personas[personas.size()-1].getPassword()<<endl;
+		
 				break;
 			}
 			case 2:{
+				string userName, Password;
+				cout<<"****LOG IN****"<<endl;
+				cout<<" -> Ingrese su nombre: ";
+				cin >> userName;
+				cout<<" -> Ingrese su contrasena: ";
+				cin>> Password;
+				bool acceso=false;
+				for(int i=0; i< personas.size(); i++){
+					if(personas[i].getNombre() == userName && personas[i].getPassword() == Password){
+						acceso=true;
+						personaIngresada=i;
+						break;
+					}else{
+						acceso=false;
+					}
+				}
+				
+				if(acceso==true){
+					int option;
+					cout<<"	***Bienvenido: "<< personas[personaIngresada].getNombre()<<"***"<<endl;
+					cout<<"(1) Enviar mensaje"<<endl;
+					cout<<"(2) Ver mensajes"<<endl;
+					cout<<"(3) Ver mi llave"<<endl;
+					cout<<"(0) Salir"<<endl;
+					cout<<"->Ingrese su opcion: "<<endl;
+					cin>>option;
+					
+				}else{
+					
+				}
 				
 			
 				
