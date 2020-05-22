5eeda6fd3047265df74cc8bf9469852c8f3ab561
Juda <judaponce14@unitec.edu>
2020-05-22 16:24:54 -0600

    avanzando

--- P3Lab4_JudaPonc.cpp
+++ P3Lab4_JudaPonc.cpp
@@ -27,12 +27,13 @@ int main(int argc, char** argv) {
 			cout<<" -> Ingrese su contrasena: ";
 			cin>> password;
 			cout<<" Ha sido registrado con exito: "<<endl;
+			cout<<endl;
 			personas.push_back(persona(nombre,apellido,password));	
 			cout<<" Su usuario es: "<<endl;
 			cout << " Nombre: "<<personas[personas.size()-1].getNombre()<<endl;
 			cout << " Apellido: "<<personas[personas.size()-1].getApellido()<<endl;
 			cout << " Contrasena: "<<personas[personas.size()-1].getPassword()<<endl;
-		
+			cout<<endl;
 				break;
 			}
 			case 2:{
@@ -59,12 +60,36 @@ int main(int argc, char** argv) {
 					cout<<"(1) Enviar mensaje"<<endl;
 					cout<<"(2) Ver mensajes"<<endl;
 					cout<<"(3) Ver mi llave"<<endl;
-					cout<<"(0) Salir"<<endl;
+					cout<<"(4) Salir"<<endl;
 					cout<<"->Ingrese su opcion: "<<endl;
 					cin>>option;
 					
+					switch(option){
+						case 1:{
+							
+							break;
+						}
+						
+						case 2: {
+							
+							
+							break;
+						}
+						
+						
+						case 3:{
+							cout<<"-!- Su llave es: ["<<personas[personaIngresada].getKey()<<"]"<<endl;
+							break;
+						}
+						
+						case 4:{
+							break;
+							break;
+						}
+					}
 				}else{
-					
+					cout<<" Nombre y/o contraseña incorrectos"<<endl;
+					cout<<endl;
 				}
 				
 			
